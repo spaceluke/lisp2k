4ca031a5fe5ee723eb815715b57ffc474a65c83f
radicalrafi <radicalrafi@gmail.com>
2016-11-19 15:29:07 +0000

    Added Error Handling for / and %

--- src/prompt.c
+++ src/prompt.c
@@ -4,32 +4,95 @@
 #include<editline/history.h>
 
 #include "mpc.h"
+/*Error handling and thought leading 
+  we handle errors up to schrodinbug level
+  your bug exist and your bug doesnt exist,program runs
+*/
+typedef struct {
+    int type;
+    long num;
+    int err;
+}lval;
+/*enumerate possible eval types*/
+enum {LVAL_NUM,LVAL_ERR};
+/*enumerate possible lval error types*/
+enum {LERR_DIV_ZERO, LERR_BAD_NUM,LERR_BAD_OP};
 
+lval lval_num(long x)
+{
+    lval v;
+    v.type=LVAL_NUM;
+    v.num=x;
+    return v;
+}
+
+lval lval_err(int x)
+{
+    lval v;
+    v.type=LVAL_ERR;
+    v.num=x;
+    return v;
+}
+
+/* print an lval */
+void lval_print(lval v)
+{
+    switch(v.type)
+    {
+        /*case it's a number print it leave..*/
+        case LVAL_NUM:printf("%li",v.num); break;
+        /*case if it's an error type we print resp log*/
+        case LVAL_ERR:
+            /*check the error type*/
+            if(v.err = LERR_DIV_ZERO){
+                puts("Error: Division by Zero");
+            }
+           if(v.err = LERR_BAD_OP){
+                puts("Error : Bad Operator"); 
+           }
+           if(v.err = LERR_BAD_NUM){
+                puts("Error: Bad Number ");
+            }    
+        break;
+    }
+}
 /*evaluate the operator against arithmetic operation*/
-long eval_op(long x, char *op, long y){
-    if(strcmp(op, "+")==0){return x+y;}
-    if(strcmp(op, "-")==0){return x-y;}
-    if(strcmp(op, "*")==0){return x*y;}
-    if(strcmp(op, "/")==0){return x/y;}
-    if(strcmp(op, "%")==0){return x%y;}
+lval eval_op(lval x, char *op, lval y){
+    
+    if(x.type == LVAL_ERR) {return x;}
+    if(y.type == LVAL_ERR) {return y;}
+    
+    if(strcmp(op, "+")==0){return lval_num(x.num+y.num);}
+    if(strcmp(op, "-")==0){return lval_num(x.num-y.num);}
+    if(strcmp(op, "*")==0){return lval_num(x.num*y.num);}
+    if(strcmp(op, "/")==0){
+        return y.num == 0
+            ? lval_err(LERR_DIV_ZERO) : lval_err(x.num/y.num) ;
+        }
 
-    return 0;
+    if(strcmp(op, "%")==0){
+        return y.num == 0
+            ? lval_err(LERR_DIV_ZERO) : lval_err(x.num % y.num);
+        }
+    return lval_err(LERR_BAD_OP);
 }
 
 /*evaluation of the regular expression*/
 
-long eval(mpc_ast_t* t)
+lval eval(mpc_ast_t* t)
 {
     /* if tagged as numeric return to it directly*/
     if(strstr(t->tag, "number")){
-        return atoi(t->contents);
+        errno = 0;
+        long x = strtol(t->contents,NULL,10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
     }
 
     /*the operator is always a second child in the tree*/
     char* op = t->children[1]->contents;
 
     /*we store the third child in x*/
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     /* iterate trough the remaining children*/
     int i = 3;
@@ -76,8 +139,8 @@ int main(int argc,char** argv)
             //if parsing is successful we print the output
             //mpc_ast_print(r.output);
             //mpc_ast_delete(r.output);
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_print(result);
             mpc_ast_delete(r.output);
 
         }else{
